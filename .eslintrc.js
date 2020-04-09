494c446c2369a388ba5586542fd6ba19f5515ffa
Duncan Giles <duncan3142@gmail.com>
2020-04-09 10:57:54 +0100

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:jest/recommended',
+    'plugin:@typescript-eslint/eslint-recommended', // Disable ESLint rules from eslint:recommended that would conflict with @typescript-eslint/recommended
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:promise/recommended',
+    'prettier/@typescript-eslint', // Disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors.
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['prettier'],
+  rules: {},
+};
---
+++ pad.spec.ts
@@ -0,0 +1,7 @@
+import { solution } from './pad';
+
+describe('solution', () => {
+  it('should return 0', () => {
+    expect(solution()).toEqual(0);
+  });
+});
