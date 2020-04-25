4ff643c45191171f09540e20391ad7a7c3f07ef0
Duncan Giles <duncan3142@gmail.com>
2020-04-25 16:18:51 +0100

    add airbnb eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,13 @@ module.exports = {
     jest: true,
   },
   extends: [
-    'eslint:recommended',
+		'eslint:recommended',
+		'plugin:promise/recommended',
     'plugin:jest/recommended',
     'plugin:@typescript-eslint/eslint-recommended', // Disable ESLint rules from eslint:recommended that would conflict with @typescript-eslint/recommended
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:promise/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'airbnb-typescript/base',
     'prettier/@typescript-eslint', // Disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors.
   ],
