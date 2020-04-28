961bb4a84c76c7465d67c95bc1093695be1a217a
Duncan Giles <duncan3142@gmail.com>
2020-04-28 14:26:08 +0100

    solitaire dynamic

--- pad.spec.ts
+++ pad.spec.ts
@@ -1,7 +1,16 @@
-import { solution } from './pad';
+import solution from './pad';
 
 describe('solution', () => {
-  it('should return 0', () => {
-    expect(solution()).toEqual(0);
-  });
+	it('should pass case 0', () => {
+		const A = [1, -2, 0, 9, -1, -2];
+		expect(solution(A)).toEqual(8);
+	});
+	it('should pass case 1', () => {
+		const A = [1, -2];
+		expect(solution(A)).toEqual(-1);
+	});
+	it('should pass case 3', () => {
+		const A = [1, -2, 4, -3, -9, 8, 11, 54, -60];
+		expect(solution(A)).toEqual(18);
+	});
 });
--- pad.ts
+++ pad.ts
@@ -1,6 +1,16 @@
-function solution(): number {
-  // write your code in JavaScript (Node.js 8.9.4)
-  return 0;
+export default function solution(A: Array<number>): number {
+	const squareOptimum = Array<number>(A.length).fill(NaN);
+	const firstSquare = A[0];
+	squareOptimum[0] = firstSquare;
+	squareOptimum.forEach((baseValue, squareIndex) => {
+		const moveRange = Math.min(A.length - squareIndex - 1, 6);
+		for (let moveDist = 1; moveDist <= moveRange; moveDist += 1) {
+			const moveToIndex = squareIndex + moveDist;
+			const nextVal = baseValue + A[moveToIndex];
+			if (Number.isNaN(squareOptimum[moveToIndex]) || squareOptimum[moveToIndex] < nextVal) {
+				squareOptimum[moveToIndex] = nextVal;
+			}
+		}
+	});
+	return squareOptimum[squareOptimum.length - 1];
 }
-
-export { solution };
